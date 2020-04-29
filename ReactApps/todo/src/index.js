364bc0a2d9f740a953a7a507cc6c27691c8035f1
Andrew <andrewerm@yandex.ru>
2020-04-29 17:18:34 +0300

    first commit

--- ReactApps/todo/src/index.js
+++ ReactApps/todo/src/index.js
@@ -1,1 +1,45 @@
-console.log(' Hi to all')
+import React from "react";
+import ReactDOM from 'react-dom'
+// import Todolist from './todolist'
+
+
+ const Todolist=()=> {
+    const items=['Learn React', 'Build Awesome App']
+
+
+    return(
+        <ul>
+
+            <li>{items[0]}</li>
+            <li>{items[1]}</li>
+        </ul>
+    )
+}
+
+const AppHeader=()=> {
+    return <h1> My ToDo list </h1>
+}
+
+const SearchPanel=()=> {
+    const searchText='search'
+    const searchStyle={
+        fontSize: '25px'
+    }
+    return <input
+        style={searchStyle}
+        placeholder={searchText}/>
+
+}
+
+const App=()=> {
+    return (
+        <div>
+            <AppHeader/>
+
+            <SearchPanel/>
+            <Todolist/>
+        </div>
+    )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
---
+++ ReactApps/todo/src/todolist.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Todolist=()=> {
+
+    return(
+        <ul>
+            <li>Learn React</li>
+            <li>Build Awesome App</li>
+        </ul>
+    )
+}
